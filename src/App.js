c4f43c5a1fab100a99eb2a99d106083ea28d86cc
trongtuan99er <trongtuan681@gmail.com>
2022-06-04 14:44:30 +0700

    init project with router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import { Navbar, HomePage, CryptoCurrencies, Exchanges, CryptoDetails, News } from "./components";
+import './App.css'
+import 'antd/dist/antd.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+      <div className="app">
+        <div className="navbar">
+          <Navbar/>
+        </div>
+        <div className="main">
+          <Layout>
+              <div className="routes">
+                <Routes>
+                  <Route exact path="/" element={<HomePage/>}/>
+                  <Route exact path="/cryptocurrencies" element={  <CryptoCurrencies/>}/>
+                  <Route exact path="/exchanges" element={<Exchanges/>}/>
+                  <Route exact path="/crypto/:coinId" element={<CryptoDetails/>}/>
+                  <Route exact path="/news" element={<News/>}/>
+                </Routes>
+              </div>
+          </Layout>
+        </div>
+        <div className="footer">
+          
+          </div>
+
+      </div>
+  )
 }
 
-export default App;
+export default App
