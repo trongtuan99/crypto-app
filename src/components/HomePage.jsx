6d6dc4507ae61d988713212ed92c8b4478925159
trongtuan99er <trongtuan681@gmail.com>
2022-06-04 17:33:22 +0700

    get api from rapidApi

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import millify from 'millify'
 import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
-
+import { useGetCryptosQuery } from '../services/cryptoApi'
 const {Title} = Typography 
 const HomePage = () => {  
+  const {data, isFetching } = useGetCryptosQuery()
+
+  console.log(data);
+
   return (
     <>
       <Title level={2} className='heading'>Tiền điện tử toàn cầu</Title>
       <Row>
         <Col span={12}> <Statistic title="Tổng số tiền điện tử" value={5}/></Col>
         <Col span={12}> <Statistic title="Tổng số Giao dịch" value={5}/></Col>
-        <Col span={12}> <Statistic title="Tổng vốn hóa thị trường " value={5}/></Col>
+        <Col span={12}> <Statistic title="Tổng vốn hóa thị trường" value={5}/></Col>
         <Col span={12}> <Statistic title="Tổng khối lượng 24h" value={5}/></Col>
         <Col span={12}> <Statistic title="Tổng số thị trường" value={5}/></Col>
       </Row>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,10 +1,10 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
   'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
   'X-RapidAPI-Key': 'ddd55aabe4msh4bbd832a9c24ac9p1d2c36jsn4b912ba14c6a'
 }
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
 const createRequest = (url) => ({url, headers:cryptoApiHeaders })
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
@@ -15,20 +15,6 @@ export const cryptoApi = createApi({
     })
   })
 });
-const options = {
-  method: 'GET',
-  url: 'https://coinranking1.p.rapidapi.com/coins',
-  params: {
-    referenceCurrencyUuid: 'yhjMzLPhuIDl',
-    timePeriod: '24h',
-    'tiers[0]': '1',
-    orderBy: 'marketCap',
-    orderDirection: 'desc',
-    limit: '50',
-    offset: '0'
-  },
-  headers: {
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': 'ddd55aabe4msh4bbd832a9c24ac9p1d2c36jsn4b912ba14c6a'
-  }
-};
+export const {
+  useGetCryptosQuery
+} = cryptoApi
